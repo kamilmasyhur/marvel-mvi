0649f17204a5d4e4549543dd9b5dfe85b2d0cc53
yuzwan19 <yuzwan19@gmail.com>
2021-08-22 22:48:24 +0700

    Add layout for detail activity

---
+++ app/src/main/java/com/yk/marvelcomics/feature/detail/presentation/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.yk.marvelcomics.feature.detail.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yk.marvelcomics.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.marvel_activity_detail)
+    }
+}
