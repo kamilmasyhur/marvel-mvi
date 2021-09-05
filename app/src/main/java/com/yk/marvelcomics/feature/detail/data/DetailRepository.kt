1a682cd355efd4d78d5ea6dbf9781e798b792d61
yuzwan19 <yuzwan19@gmail.com>
2021-09-05 23:05:41 +0700

    Create Detail Repository

--- app/src/main/java/com/yk/marvelcomics/feature/detail/data/DetailRepository.kt
+++ app/src/main/java/com/yk/marvelcomics/feature/detail/data/DetailRepository.kt
@@ -1,14 +1,50 @@
 package com.yk.marvelcomics.feature.detail.data
 
+import com.yk.marvelcomics.feature.detail.data.response.DetailResponse
 import com.yk.marvelcomics.repository.MarvelApi
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.Path
 import javax.inject.Inject
 
 interface DetailRepository {
-
+    fun getDetailComic(comicId: Int): Single<DetailResponse>
+    fun getCharacterByComicId(comicId: Int): Single<DetailResponse>
+    fun getDetailCharacter(characterId: Int): Single<DetailResponse>
+    fun getComicsByCharacterId(characterId: Int): Single<DetailResponse>
+    fun getDetailEvent(eventId: Int): Single<DetailResponse>
+    fun getCharactersByEventId(eventId: Int): Single<DetailResponse>
+    fun getComicsByEventId(eventId: Int): Single<DetailResponse>
 }
 
 class DetailRepositoryImpl @Inject constructor(
     private val api: MarvelApi
 ) : DetailRepository {
+    override fun getDetailComic(comicId: Int): Single<DetailResponse> {
+        return api.getDetailComic(comicId)
+    }
+
+    override fun getCharacterByComicId(comicId: Int): Single<DetailResponse> {
+        return api.getDetailCharacter(comicId)
+    }
+
+    override fun getDetailCharacter(characterId: Int): Single<DetailResponse> {
+        return api.getDetailCharacter(characterId)
+    }
+
+    override fun getComicsByCharacterId(characterId: Int): Single<DetailResponse> {
+        return api.getComicsByCharacterId(characterId)
+    }
+
+    override fun getDetailEvent(eventId: Int): Single<DetailResponse> {
+        return api.getDetailEvent(eventId)
+    }
+
+    override fun getCharactersByEventId(eventId: Int): Single<DetailResponse> {
+        return api.getCharactersByEventId(eventId)
+    }
+
+    override fun getComicsByEventId(eventId: Int): Single<DetailResponse> {
+        return api.getComicsByEventId(eventId)
+    }
 
 }
--- app/src/main/java/com/yk/marvelcomics/repository/MarvelApi.kt
+++ app/src/main/java/com/yk/marvelcomics/repository/MarvelApi.kt
@@ -1,10 +1,12 @@
 package com.yk.marvelcomics.repository
 
+import com.yk.marvelcomics.feature.detail.data.response.DetailResponse
 import com.yk.marvelcomics.feature.home.data.response.CharactersResponse
 import com.yk.marvelcomics.feature.home.data.response.ComicsResponse
 import com.yk.marvelcomics.feature.home.data.response.EventsResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MarvelApi {
     @GET("/v1/public/comics")
@@ -15,4 +17,40 @@ interface MarvelApi {
 
     @GET("/v1/public/events")
     fun getEvents(): Single<EventsResponse>
+
+    @GET("/v1/public/comics/{comicId}")
+    fun getDetailComic(
+        @Path("comicId") comicId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/comics/{comicId}/characters")
+    fun getCharacterByComicId(
+        @Path("comicId") comicId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/characters/{characterId}")
+    fun getDetailCharacter(
+        @Path("characterId") characterId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/characters/{characterId}/comics")
+    fun getComicsByCharacterId(
+        @Path("characterId") characterId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/events/{eventId}")
+    fun getDetailEvent(
+        @Path("eventId") eventId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/events/{eventId}/characters")
+    fun getCharactersByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<DetailResponse>
+
+    @GET("/v1/public/events/{eventId}/comics")
+    fun getComicsByEventId(
+        @Path("eventId") eventId: Int
+    ): Single<DetailResponse>
+
 }
